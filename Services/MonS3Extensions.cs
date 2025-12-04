4e72b7cdd200ff84d461fce93c3e43fa58cb6c84
JimGH75 <github@sysinfo.cz>
2025-12-04 22:29:08 +0100

    Add files via upload

---
+++ Services/MonS3Extensions.cs
@@ -0,0 +1,16 @@
+﻿using MonS3API;
+
+public static class MonS3Extensions
+{
+    public static int AsInt(this MonS3APIValue v)
+        => Convert.ToInt32(v);
+
+    public static decimal AsDecimal(this MonS3APIValue v)
+        => Convert.ToDecimal(v);
+
+    public static string AsString(this MonS3APIValue v)
+        => v?.ToString();
+
+    public static DateTime AsDate(this MonS3APIValue v)
+        => Convert.ToDateTime(v);
+}
